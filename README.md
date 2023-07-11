f17aaa2a4869e456ce987f305cd6633a81228e2c
javanasse <julian.vanasse@gmail.com>
2023-07-11 19:24:14 -0400

    finished validating plugins using pluginval

--- README.md
+++ README.md
@@ -2,7 +2,27 @@
 
 Host audio VSTs and audio processing on a web server.
 
-```python
-cd app
-uvicorn server:app --host 0.0.0.0 --port 8080
+## Installation
+Install requirements.
+```console
+pip install -r requirements.txt
+```
+
+### Installing `pluginval`
+To validate VSTs we use a tool developed by Tracktion called `pluginval`. This is a contraction of plugin validator, and is used to determine if a VST will run on the system of choice.
+#### MacOS
+```console
+curl -L "https://github.com/Tracktion/pluginval/releases/download/latest_release/pluginval_macOS.zip" -o pluginval.zip
+unzip pluginval.zip
+```
+#### Linux
+```console
+curl -L "https://github.com/Tracktion/pluginval/releases/download/latest_release/pluginval_linux.zip" -o pluginval.zip
+unzip pluginval.zip
+```
+
+## Start Server
+Start the server by running the following command.
+```console
+python3 app/server.py
 ```
--- app/plugins/plugins.py
+++ app/plugins/plugins.py
@@ -1,4 +1,4 @@
-import os, glob
+import os, glob, subprocess
 from plugins.VST import VST
 from config import PLUGINS_DIR
 
@@ -24,7 +24,31 @@ def list_vst_files():
     result = [y for x in os.walk(PLUGINS_DIR) for y in glob.glob(os.path.join(x[0], '*.vst3'))]
     return result
 
-def list_vsts():
+def validate_vsts(files, timeout_duration=15):
+    """
+    Validates all plugins using `pluginval` application.
+    """
+    valid_files = list()
+    for file in files:
+        validator = subprocess.Popen(["./pluginval.app/Contents/MacOS/pluginval", 
+                      "--validate-in-process", 
+                      "--verbose"
+                      "--strictness-level", "5",
+                      "--validate",
+                      f"{file}"])
+        try:
+            validator.wait(timeout=timeout_duration)
+            if validator.returncode == 0:
+                valid_files.append(file)
+            else:
+                print("This plugin ain't worth the toilet-paper it's smeared on.")
+        except subprocess.TimeoutExpired:
+            print(f"{file} is invalid.")
+            validator.kill()
+
+    return valid_files
+
+def load_vsts(valid_files):
     vst_files = list_vst_files()
     result = dict()
     for file in vst_files:
@@ -35,4 +59,8 @@ def list_vsts():
         
 
 
-VST_LIST = list_vsts()
+VST_LIST = load_vsts(
+    validate_vsts(
+        list_vst_files()
+    )
+)
